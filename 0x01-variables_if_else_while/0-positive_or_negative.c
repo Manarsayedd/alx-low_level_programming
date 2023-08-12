efd8fe3606d0293c474ee895b4e38c438e48da36
Manarsayedd <manarsayed67@gmail.com>
2023-08-12 23:47:34 +0000

    kjf

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,16 @@ srand(time(NULL));
 /* Generate a random number. */
 n = rand() - RAND_MAX / 2;
 /* Check if the number is positive, negative, or zero. */
-if (n > 0) {
+if (n > 0)
+{
 printf("%i is positive\n", n);
-} else if (n == 0) {
+} 
+else if (n == 0)
+{
 printf("%i is zero\n", n);
-} else {
+}
+else
+{
 printf("%i is negative\n", n);
 }
 return (0);
