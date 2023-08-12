590fa3e86f503adb6c3473436ddddfbae6057e70
Manarsayedd <manarsayed67@gmail.com>
2023-08-12 01:58:37 +0000

    hgb

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,13 +19,13 @@ if(n>0)
 {
 	printf("%i is positive \n",n);
 }
-else if(n<0)
+else if(n == 0)
 {
-	printf("%i is negative \n",n);
+	printf("%i is zero \n",n);
 }
 else
 {
-	printf("%i is zero \n",n);
+	printf("%i is negative \n",n);
 }
 return (0);
 }
