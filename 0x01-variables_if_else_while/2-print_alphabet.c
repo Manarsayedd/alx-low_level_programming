d7071023b5bae597996058772076d8de3c9e1b5e
Manarsayedd <manarsayed67@gmail.com>
2023-08-13 18:19:59 +0000

    kjf

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: outsput all the alphabet letters in lowercase
+ *
+ * return : always 0 (success)
+ */
+int main(void)
+{
+for (char i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
+
+
