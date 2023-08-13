8589bd5e44e5bf12411b406bd0a2dc4e0eeb9f7a
Manarsayedd <manarsayed67@gmail.com>
2023-08-13 19:15:27 +0000

    kjf

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: outsput all the alphaabet letters except e and q in lowercase
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+puts(i);
+}
+putchar("\n");
+return (0);
+}
