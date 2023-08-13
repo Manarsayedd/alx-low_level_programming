c0cd2bc8d4871178a908b327570a27e00dfd3993
Manarsayedd <manarsayed67@gmail.com>
2023-08-13 19:48:09 +0000

    kjf

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all numbers with base 16
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int i;
+char s;
+for (i = 0 ; i < 10; i++)
+{
+putchar((i % 10) + '0');
+}
+for (s = 'a' ; i <= 'f'; i++)
+{
+putchar(s);
+}
+return (0);
+}
