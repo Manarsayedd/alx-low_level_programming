3f07c30ed898b46979272290f8a680127eb349c6
Manarsayedd <manarsayed67@gmail.com>
2023-08-13 18:24:50 +0000

    kjf

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ n = rand() - RAND_MAX / 2;
 digit = n % 10;
 if (digit > 5)
 {
-printf("last digit of %i is %i and is greater than 5", n, digit)
+printf("last digit of %i is %i and is greater than 5", n, digit);
 }
 else if (digit == 0)
 {
-printf("last digit of %i is %i and is 0", n, digit)
+printf("last digit of %i is %i and is 0", n, digit);
 }
-else if (digit < 6 && digit != 0)
+else if (digit < 6 && digit != 0);
 {
-printf("last digit of %i is %i and is less than 6 and not 0", n, digit)
+printf("last digit of %i is %i and is less than 6 and not 0", n, digit);
 }
 return (0);
 }
