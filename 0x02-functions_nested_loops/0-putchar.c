46c264a4c678f8ac31829cd2bb16b3568fc85ce6
Manarsayedd <manarsayed67@gmail.com>
2023-08-16 23:46:35 +0000

    dihg

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,12 +10,12 @@
  */
 int main(void)
 {
-char ch[8] = "_putchar";
+char ch[] = "_putchar";
 int i;
 for (i = 0; i < 8; i++)
 {
 _putchar(ch[i]);
-putchar('\n');
+_putchar('\n');
 }
 return (0);
 }
