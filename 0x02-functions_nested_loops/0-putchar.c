15d1b10e0b1bd064675dacaa6551283491c90b99
Manarsayedd <manarsayed67@gmail.com>
2023-08-16 02:35:48 +0000

    df

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-echo"_putchar"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: prints _putchar
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+printf("_putchar");
+return 0;
+}
