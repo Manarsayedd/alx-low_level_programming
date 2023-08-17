c452dd5f0bfaec671c84892444c6a0622713aff5
Manarsayedd <manarsayed67@gmail.com>
2023-08-17 01:45:52 +0000

    kjb

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,17 +15,18 @@ int print_sign(int n)
 {
 if (n >= 0 && n <= 47)
 {
-_putchar('-');
+_putchar(45);
+_putchar(49);
 return (-1);
 }
 else if (n == 48)
 {
-_putchar('0');
+_putchar(48);
 return (0);
 }
 else if (n > 48)
 {
-_putchar('1');
+_putchar(49);
 return (1);
 }
 }
