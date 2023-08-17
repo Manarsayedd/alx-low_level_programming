0899a389838fccca2f165f862ac6de95bceaf076
Manarsayedd <manarsayed67@gmail.com>
2023-08-17 01:41:16 +0000

    kjb

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - fumction prints sign of number
+ *
+ * @n: checks on the input of the function
+ * description: function that prints the sign of a number
+ *
+ * Return: will return 1 if n is between 0 and 47
+ *                     0 if n is equal 48
+ *                     -1 if n is greater than 48
+ */
+
+int print_sign(int n)
+{
+if (n >= 0 && n <= 47)
+{
+_putchar('-');
+return (-1);
+}
+else if (n == 48)
+{
+_putchar('0');
+return (0);
+}
+else if (n > 48)
+{
+_putchar('1');
+return (1);
+}
+}
