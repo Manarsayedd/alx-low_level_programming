30e91d3d3fda840b72f8c0fc86c5a04a9834ef60
Manarsayedd <manarsayed67@gmail.com>
2023-08-19 23:53:05 +0000

    dkljh

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiply 2 numbers
+ *
+ * description: a function used to multiply 2 numbers
+ * @a: first input
+ * @: second input
+ *
+ * Return: always 0 (success)
+ */
+int mul(int a, int b)
+{
+	int mutiply;
+	multiply = a * b;
+	return (multiply);
+}
