4082710cfd016680ac59e449bd827d81868fd24e
Manarsayedd <manarsayed67@gmail.com>
2023-08-20 04:10:28 +0000

    dkljh

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ */
+void main(void)
+{
+	int i;
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 != 0))
+		{
+			printf("Fizz");
+		}
+		else if ((i % 5 == 0) && (i % 3 != 0))
+		{
+			printf("Buzz");
+		}
+		else if (( i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FIzzBuzz");
+		}
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+		else
+			printf("n");
+	}
+}
+
