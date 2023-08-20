e449c5d4c1af4f3c98a7b37176021d427ae99da1
Manarsayedd <manarsayed67@gmail.com>
2023-08-20 00:24:33 +0000

    dkljh

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers 0 to 9
+ *
+ * description: a function that prints numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i);
+
+	_putchar("\n")
+}
