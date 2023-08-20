a2ce5f031a18ff592db9da2e9905144d106480bf
Manarsayedd <manarsayed67@gmail.com>
2023-08-20 02:57:57 +0000

    dkljh

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include <stio.h>
+#include "main.h"
+/**
+ * print_diagonal - prints \ n times
+ *
+ * description: function that prints \ n times
+ *
+ * @n :input of the function
+ */
+void print_diagonal(int n)
+{
+	int i, cnt = 1, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (space = 0; space < cnt; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar(95);
+			_putchar('\n');
+		}
+	}
+}
+
