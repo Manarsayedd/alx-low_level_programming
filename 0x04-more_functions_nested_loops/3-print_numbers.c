0f9c024ee8d4058c5a5705372954eb4e6cec0b10
Manarsayedd <manarsayed67@gmail.com>
2023-08-20 00:29:15 +0000

    dkljh

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,10 @@
  */
 void print_numbers(void)
 {
-	int i;
+        int i;
 
-	for (i = 0; i < 10; i++)
-		_putchar(i);
+        for (i = 0; i < 10; i++)
+                _putchar(i);
 
-	_putchar("\n")
+        _putchar("\n")
 }
