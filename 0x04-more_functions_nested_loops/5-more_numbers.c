a523b0ba33cf782b0b3f43bbdf5d074b4db4393f
Manarsayedd <manarsayed67@gmail.com>
2023-08-20 01:52:03 +0000

    dkljh

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,7 +7,7 @@
  */
 void more_numbers(void)
 {
-	int i,j;
+	int i, j, numb;
 
 	for (i = 0; i <= 10; i++)
 	{
@@ -16,12 +16,9 @@ void more_numbers(void)
 			if (j > 9)
 			{
 				_putchar(49);
-				_putchar((j % 10) + 48);
+				numb = j % 10;
 			}
-			else
-			{
-				_putchar(j + 48);
-			}
+			_putchar(j + 48);
 		}
 		_putchar('\n');
 	}
