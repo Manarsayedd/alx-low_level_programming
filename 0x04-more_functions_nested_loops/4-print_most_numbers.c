7e891150ae5d7322a9a1990c814da00290bb19a3
Manarsayedd <manarsayed67@gmail.com>
2023-08-20 01:03:23 +0000

    dkljh

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers except 2 and 4
+ *
+ * description: functions prints numbers from 0 to 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		else
+			_putchar(i + 48);
+	}
+	_putchar('\n');
+}
