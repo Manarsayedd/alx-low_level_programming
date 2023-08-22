82d900f627675ccfcbf1fae721f1dcb257a2210a
Manarsayedd <manarsayed67@gmail.com>
2023-08-22 03:28:24 +0000

    vj

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,15 +13,12 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
 		if (n - i != 1)
 		{
-			_putchar(",");
-			_putchar(" ");
-		}
+			printf("%d, ", a[i]);
 		else
 		{
-			break;
+			printf("%d", a[i]);
 		}
 	}
 	_putchar('\n');
