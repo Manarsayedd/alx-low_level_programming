d23a4b2dd73d10d5be8ef519d78367f15e8117c3
Manarsayedd <manarsayed67@gmail.com>
2023-08-22 01:11:52 +0000

    vj

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - outputs a string
+ *
+ * description: used to output the string pointed to
+ * @str: input of the function
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+}
