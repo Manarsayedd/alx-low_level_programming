6465e0b4b23887a554c32af69016989c837c3846
Manarsayedd <manarsayed67@gmail.com>
2023-08-22 03:54:57 +0000

    vj

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_strcpy - copy char array from another array
+ * description: copy char array from another array
+ * @dest: first input
+ * @src: second input
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0')
+	return (dest);
+}
