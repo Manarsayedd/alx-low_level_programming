e4a736af119252c785fb917873df338ca6feb240
Manarsayedd <manarsayed67@gmail.com>
2023-08-25 01:31:26 +0000

    jb

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,30 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * _strncat - function to concatenates 2 string
- *
- * description: used to concatenates 2 strings
- * Return: alwyas 0 (success)
- * @dest: first input
- * @src: seconf input
+ * *_strncat - appends n elements from a second string to a first
+ * @dest: pointer to string dest
+ * @src: pointer to original string
+ * @n: number of elements
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int c = 0, c2;
-	while (dest[c] != '\0')
-		c++;
-	for (c2 = 0; c2 < n, c2++)
-	{
-		dest[c++] = src[c2];
-	}
-	return (*dest)
+int i, j, k;
+
+i = j = k = 0;
+
+while (*(dest + i) != '\0')
+i++;
+while (*(src + j) != '\0')
+j++;
+if (n > j)
+n = j;
+for (; k < n; k++)
+{
+*(dest + i) = *(src + k);
+i++;
+															}
+*(dest + i) = '\0';
+
+return (dest);
 }
