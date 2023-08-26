0974c5cdbf4f96161c8f21209db5748faca98d21
Manarsayedd <manarsayed67@gmail.com>
2023-08-26 02:31:08 +0000

    hg

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main .h"
+/**
+ *_strcmp - compares
+ * 
+ * description: compares
+ *
+ * @s1: first 
+ * @s2: second 
+ * Return: negative wewewe
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int ret, c = 0, c2 = 0;
+
+	while (s1[c] != '\0')
+		c++;
+	while (s2[c2] != '\0')
+		c2++;
+	if (c < c2)
+		ret = -15;
+	else if (c == c2)
+		ret = 0;
+	else
+		ret = 15;
+}
+
+
