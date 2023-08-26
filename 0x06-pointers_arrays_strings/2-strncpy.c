98f625aa83052a2322ad71e947c9f23497dd36bf
Manarsayedd <manarsayed67@gmail.com>
2023-08-26 01:50:06 +0000

    hg

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* _strcpy - copies a string
+* _strncpy - copies a string
 * description: copies a string
-* @dest - first input
+* @dest: first input
 * @src: seconf input
 * @n: third input
 * Return: dest
@@ -11,42 +11,22 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-        int c = 0, c2 = 0;
+	int c = 0, c2 = 0;
 
-        while (src[c2] != '\0')
-                c2++;
-                
-        if (n > c2)
-                n = c2;
+	while (src[c2] != '\0')
+		c2++;
 
-        for (c = 0; c < n; c++)
-        {
-                dest[c] = src[c];
-        }
-        
-        while (c2 < n)
-        {
-                dest[c] = '\0';
-                c++;
-        }
+	if (n > c2)
+		n = c2;
 
-        return (dest);
-}
-int main(void)
-{
-    char s1[98];
-    char *ptr;
-    int i;
-
-    for (i = 0; i < 98 - 1; i++)
-    {
-        s1[i] = '*';
-    }
-    s1[i] = '\0';
-    printf("%s\n", s1);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-    printf("%s\n", s1);
-    printf("%s\n", ptr);
-    
-    return (0);
+	for (c = 0; c < n; c++)
+	{
+		dest[c] = src[c];
+	}
+	while (c2 < n)
+	{
+		dest[c] = '\0';
+		c++;
+	}
+	return (dest);
 }
