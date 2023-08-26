21cccd54a5dec1dc0efa1df29d277b12c904d98c
Manarsayedd <manarsayed67@gmail.com>
2023-08-26 01:11:02 +0000

    hg

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * _strncpy - copies a astring
- * description: copies a string
+ * descripition: copies a string
  * @dest: first input
  * @src: second input
- * @n:third input 
- * return: dest
+ * @n: third input
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -25,4 +25,4 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 
 	return (dest);
-}	
+}
