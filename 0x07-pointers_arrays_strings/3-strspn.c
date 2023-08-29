8289f3ab94bdfc9fd59f2dfe4ffc58ebcd07f494
Manarsayedd <manarsayed67@gmail.com>
2023-08-29 02:04:12 +0000

    hc

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - gets the length of the prefix
+ * @s: first input
+ * @accept: second input
+ * Return: cnt
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int cnt = 0, i, j;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[j] == accept[i])
+			{
+				cnt += 1;
+				break;
+			}
+		}
+	}
+	if (accept[i] == '\0')
+		cnt += 1;
+	return (cnt);
+}
+
