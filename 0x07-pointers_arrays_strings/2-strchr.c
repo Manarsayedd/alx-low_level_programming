8e58ef9fafa30e8491c6b2eae4ba4c9686122276
Manarsayedd <manarsayed67@gmail.com>
2023-08-29 01:42:27 +0000

    hc

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*_strchr - indicates if the letter we are searching for is in the string
+* _strchr - indicates if the letter we are searching for is in the string
 * @s: first input
 * @c: seconf input
 * Return: if found s[j]
