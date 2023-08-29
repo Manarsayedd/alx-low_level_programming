4839345e1abac80b3cc373c26d28b42c696a80a3
Manarsayedd <manarsayed67@gmail.com>
2023-08-29 00:58:33 +0000

    hc

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_memcpy - set memory with constant bytes wewewewe wewewewe
+* @dest: first input
+* @src: second input
+* @n: third input
+* Return: dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,4 +4,6 @@
 #include <stdio.h>
 int _putchar(int c);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
 #endif
