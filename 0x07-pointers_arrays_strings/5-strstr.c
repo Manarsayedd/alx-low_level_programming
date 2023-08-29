a9cad7f602f37a8cfb56146199c23afa31cf3c2a
Manarsayedd <manarsayed67@gmail.com>
2023-08-29 02:48:23 +0000

    hc

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *t;
+	while (needle[i] != '\0')
+	{
+		j = 0;
+		while (haystack[j] != 0)
+		{
+			if ( 
