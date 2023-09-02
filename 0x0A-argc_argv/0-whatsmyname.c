abca49a0b84ef42b3c26952609eb1a995d17000d
Manarsayedd <manarsayed67@gmail.com>
2023-09-02 00:21:51 +0000

    jhg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,13 @@
 /**
  * main - main entry
  * Return: 0 (success)
+ * @__attribute__((unused)): wewewejhg jkgjh kyguh kgjd
+ * @ *argv[]: kjnci iudhpiun iojnipn pidnpi
+ * @argc: ioughdoiuh iduhu iodjbhu
+ * @argv: jhdgbkjhb kjdhbk udhb
  */
-int main(int argc __atribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s \n", argv[0]);
-	return 0;
+	return (0);
 }
