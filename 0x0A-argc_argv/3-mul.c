44f3e1f572f3dedc3c8c4f22c34cc5784f67e3b1
Manarsayedd <manarsayed67@gmail.com>
2023-09-02 01:50:43 +0000

    jhg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,23 +1,55 @@
 #include "main.h"
 /**
+ * _atoi - function kjhjkdb lksjbj
+ * @s: first input
+ * Return: zeft teen
+ */
+int _atoi(char *s)
+{
+	short ngtv = 1;
+	unsigned int num = 0, i = 0;
+	int finalnum = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 48 && *(s + i) <= 57)
+		{
+			num = (num * 10) + (*(s + i) - 48);
+			if (!(*(s + i + 1) >= 48 && *(s + i + 1) <= 57))
+				break;
+		}
+		if (*(s + i) == '-')
+		{
+			ngtv *= -1;
+			i++;
+		}
+		if (ngtv == -1)
+			finalnum = num * ngtv;
+		else
+			finalnum = num;
+		return (finalnum);
+	}
+}
+/**
  * main - entrance point
- * @argc: odhg kldjn dkjb
- * @argv: khdg idhu kdhg
+ * @argc: djh idhil lkdjh
+ * @argv: pdkjh lkjd
  * Return: 0 (success)
  */
 int main(int argc, char *argv[])
 {
+	int v1, v2;
 
+	v1 = _atoi(argv[1]);
+	v2 = _atoi(argv[2]);
 	if (argc < 3)
 	{
-		printf("ERROR\n");
+		printf("ERROR \n");
 		return (1);
 	}
 	else
 	{
-		printf("%d\n", atoi(argv[argv[1]] * argv[2]));
+		printf("%d\n", v1 * v2);
 		return (0);
 	}
 }
-
-
