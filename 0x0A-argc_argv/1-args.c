31abf13ac3787d0c97f071fc5cd24cdb8a3ee14f
Manarsayedd <manarsayed67@gmail.com>
2023-09-02 00:41:45 +0000

    jhg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,9 +5,8 @@
  * @argv: kdh hdljh lehdl hdbjbh kjdl
  * Return: 0 (success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	void (*argv[]);
 	printf("%d\n", argc - 1);
 	return 0;
 }
