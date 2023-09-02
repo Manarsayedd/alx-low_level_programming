68135c7bfbb87d7752a0b27ec9e0a53d137e814f
Manarsayedd <manarsayed67@gmail.com>
2023-09-02 00:23:13 +0000

    jhg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
  * main - main entry
  * Return: 0 (success)
@@ -7,8 +9,9 @@
  * @argc: ioughdoiuh iduhu iodjbhu
  * @argv: jhdgbkjhb kjdhbk udhb
  */
+
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s \n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
