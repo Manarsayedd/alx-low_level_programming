530e8062eaced790e334aecbb7a3a260c7a87d2f
Manarsayedd <manarsayed67@gmail.com>
2023-09-02 00:40:08 +0000

    jhg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - main entrance
+ * @argc: kdhgjk kjdbj kjbdh
+ * @argv: kdh hdljh lehdl hdbjbh kjdl
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	void (*argv[]);
+	printf("%d\n", argc - 1);
+	return 0;
+}
