fd831c6fe8c4bfd13bcf92a5fc19ed9a476c7532
Manarsayedd <manarsayed67@gmail.com>
2023-09-02 00:46:48 +0000

    jhg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entrnce of the code
+ * @argc: kdjhj jkdfj kdjb
+ * @argv: iduh iduh kdjnb
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
