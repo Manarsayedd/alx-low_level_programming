c002dd2333d282048135d45fefede6ea24f6df1b
Manarsayedd <manarsayed67@gmail.com>
2023-09-05 23:52:22 +0000

    khg

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,19 +9,21 @@ char *str_concat(char *s1, char *s2)
 {
 	int v1, v2, v3;
 	char *ptr;
+
+	if (s1 == NULL)
+		s1 == "";
+	if (s2 == NULL)
+		s2 = "";
 	
 	for (v1 = 0; s1[v1] != '\0'; v1++)
-	{
-		if (s1[v1] == NULL)
-			s1[v1] == "";
-	}
+		;
 	for (v2 = 0; s2[v2]; v2++)
-	{
-		if (s2[v2]== NULL)
-			s2[v2] == "";
-	}
+		;
 
 	ptr = malloc((v1 + v2) * sizeof(char) + 1);
+	if (ptr == NULL)
+		return (NULL);
+
 	for (v3 = 0; v3 < v1 + v2; v3++)
 	{
 		if (v3 < v1)
