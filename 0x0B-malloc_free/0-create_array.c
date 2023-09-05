3fdacf5605fc862b5debedaf675764dac336e636
Manarsayedd <manarsayed67@gmail.com>
2023-09-05 03:00:28 +0000

    khg

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,10 +2,13 @@
 /**
  * *create_array - function to make an array
  * Return: null if size equals 0 else it returns the pointer
+ * @size: first input
+ * @c: second input
  */
 char *create_array(unsigned int size, char c)
 {
 	char *n = malloc(size);
+
 	if (size == 0 || n == 0)
 		return (NULL);
 	while (size--)
