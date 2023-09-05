98b346f187ad32a32509afaaca66316e95cf1c09
Manarsayedd <manarsayed67@gmail.com>
2023-09-05 04:02:39 +0000

    khg

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,10 +7,11 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *n = malloc(size * sizeof (char);
+	char *n = malloc(size * sizeof (char));
 
-	if (size == 0 || n == 0)
+	if (size == 0 || n == NULL)
 		return (NULL);
+
 	while (size--)
 		n[size] = c;
 
