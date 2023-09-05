1d115fceee58eae017b0a82dc5eed75db2420ad1
Manarsayedd <manarsayed67@gmail.com>
2023-09-05 23:33:04 +0000

    khg

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,15 +1,31 @@
 #include "main.h"
 /**
- *
+ * *str_concat - function that puts two strings in one array of string
+ * @s1: first input
+ * @s2: second input
+ * Return: ptr
  */
 char *str_concat(char *s1, char *s2)
 {
 	int v1, v2, v3;
 	char *ptr;
 
+	if (s1 == NULL)
+		s1 == '\0';
+	if (s2 == NULL)
+		s2 == '\0';
+
 	for (v1 = 0; s1[v1] != '\0'; v1++)
 		;
-	for(v2 = 0; s2[v2]; v2++)
+	for (v2 = 0; s2[v2]; v2++)
 		;
-	v3 = (v1 + v2) - 1;
-	for (
+	ptr = malloc((v1 + v2) * sizeof(char) + 1);
+	for (v3 = 0; v3 < v1 + v2; v3++)
+	{
+		if (v3 < v1)
+			ptr[v3] = s1[v3];
+		else
+			ptr[v3] = s2[v3 - v1];
+	}
+	ptr[v3] = '\0';
+	return (ptr);
