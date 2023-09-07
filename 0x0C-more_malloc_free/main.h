3064ae5be51382a7c64bb7fbf2c6f3142277fdb4
Manarsayedd <manarsayed67@gmail.com>
2023-09-07 23:53:56 +0000

    kjg

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+int _putchar(char c);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+#endif
