543c0ec5cb60558a3bc97a46d53c3272df651d13
Manarsayedd <manarsayed67@gmail.com>
2023-09-08 00:02:46 +0000

    khg

--- 0x0C-more_malloc_free/0-malloc_checker.c
+++ 0x0C-more_malloc_free/0-malloc_checker.c
@@ -1,1 +1,17 @@
+#include"main.h"
 
+/**
+* *malloc_checked-  a function that allocates memory using malloc
+* @b: arguments
+* Return: pointer to array initialized or NULL
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *x;
+
+	x = malloc(b);
+
+	if (x == 0)
+		exit(98);
+	return (x);
+}
