631786ad2f15471e6ad1d14095a4dc1c45fed740
Manarsayedd <manarsayed67@gmail.com>
2023-09-09 02:54:35 +0000

    kjhb

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,8 +13,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	unsigned int i = 0, j = 0, l1 = 0, l2 = 0;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
 	while (s1 && s1[l1])
 		l1++;
 	while (s2 && s2[l2])
