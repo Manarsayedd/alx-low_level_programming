2f5794860761a31cc1b68bea02855f4f8edbec7c
Manarsayedd <manarsayed67@gmail.com>
2023-09-12 15:36:43 +0000

    hg

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,8 +8,8 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("my_dog.name = %s\n", d->name ? d->name : "(nil)");
-		printf("my_dog.age = %f\n", d->age);
-		printf("my_dog.owner = %s\n", d->owner ? d->owner : "(nil)");
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age = %f\n", d->age);
+		printf("Owner = %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
