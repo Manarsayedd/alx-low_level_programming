6c5e336d87b24112d3dd552f16d50e57ad97279c
Manarsayedd <manarsayed67@gmail.com>
2023-09-12 15:10:52 +0000

    hg

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * print_dog - prints the details oif the struct
+ * @d: first input
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("my_dog.name = %s\n", d->name ? d->name : "(nil)");
+		printf("my_dog.age = %f\n", d->age ? d->age : "(nil)");
+		printf(("my_dog.owner = %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
