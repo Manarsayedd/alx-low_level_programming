d293a78ce51387aad2aabf1389c4ad9f0aed3d63
Manarsayedd <manarsayed67@gmail.com>
2023-09-12 14:35:32 +0000

    jh

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct of a dog
+ * @name: dog's name
+ * @age: dpg's age
+ * @owner: dog's owner
+ */
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
+
