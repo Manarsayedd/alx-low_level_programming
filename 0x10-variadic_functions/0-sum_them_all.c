3314bfb98e2ac2ef3ab726ff098ffe94b9e247f4
Manarsayedd <manarsayed67@gmail.com>
2023-09-17 02:30:17 +0000

    kjh

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,7 +13,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 	int i, sum = 0;
 
-	va_start(args, count);
+	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
