c2edfc0252d53cf8a11c26f328fdabc4f4a95fbb
Manarsayedd <manarsayed67@gmail.com>
2023-09-17 03:52:54 +0000

    kjh

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,40 @@
+#include "variadic_functions.h"
+/**
+* print_strings - function prints words
+* @separator: first input
+* @n: second input
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+
+{
+	va_list args;
+	unsigned int i;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	if (separator == NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (va_arg(args, char) != NULL)
+				printf("%s", va_arg(args, char));
+			else if (va_arg(args, char) == NULL)
+				printf("(nil)");
+		}
+	}
+	else if (separator != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (va_arg(args, char) != NULL)
+				printf("%s", va_arg(args, char));
+			else if (va_arg(args, char) == NULL)
+				printf("(nil)");
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
