59424d4b1181d4c0f94e8c93ead41162901f045e
Manarsayedd <manarsayed67@gmail.com>
2023-09-17 04:45:38 +0000

    kjh

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.c"
+/**
+ * print_all - function to print charachters wewewe
+ * @format: first input
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char space = "", *str;
+	va_start(args, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch(format[i])
+			{
+				case 'c':
+					printf("%s%c", space, va_arg(args, char));
+					break;
+				case 'i':
+					printf("%s%d", space, va_arg(args, int));
+					break;
+				case 'f':
+					printf("%s%f", space, va_arg(args, float));
+					break;
+				case 's': 
+					str = va_arg(args, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", space, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			i++;
+			space = ", ";
+		}
+		printf("\n");
+		va_end(args);
+	}
+}
+
+
+
+
+
