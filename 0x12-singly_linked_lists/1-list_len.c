803f363f23f228b886358e2934a6ac80a47fe30e
Manarsayedd <manarsayed67@gmail.com>
2023-09-25 16:31:54 +0000

    kjhg

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include<stdlib>
+#include"lists.h"
+/**
+ * list_len - function to print number of nodes
+ * @h: first input
+ * Return : number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t cnt = 0;
+
+	while (h)
+	{
+		cnt ++;
+		h = h->next;
+	}
+	return (cnt);
+}
